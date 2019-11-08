782b313da771c802033a260a495d2c3ab12a2630
Ridza Adhandra <andradhandra@gmail.com>
2019-11-08 16:39:37 +0700

    exercise7 week4 mengurutkanAbjad ridza

--- ridza-exercise7-week4-mengurutkanAbjad.js
+++ ridza-exercise7-week4-mengurutkanAbjad.js
@@ -4,20 +4,28 @@ function urutkanAbjad(str) {
     // you can only write your code here!
     //tanpa menggunakan sort()
 
-    var hasil = "";
+    //mengubah string jd array
+    var arr = str.split("");
+    var sementara = "";
     
-    for (var i=0; i<str.length; i++) {
-        for (var j=0; j<str.length-1; j++) {
-            if (str[j] > str[j+1]) {
-                var sementara = str[j];
-                str[j] += str[j+1];
-                str[j+1] = sementara;
-
+    //perulangan untuk menukar huruf
+    for (var i=0; i<arr.length; i++) {
+        for (var j=0; j<arr.length-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                sementara = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = sementara;
             } 
     }
     
+  } 
+
+  //mengembalikan array to string
+  var hasil = "";
+  for (var i=0; i<arr.length; i++) {
+    hasil += arr[i];
   }
-  return str; 
+  return hasil; 
 }
 
 
